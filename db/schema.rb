ca65cd318dcb51a291cfef659bbb83308ceb7219
ryufuta <rfutago@gmail.com>
2024-05-01 22:11:53 +0900

    author列追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_01_125819) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_01_130445) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
 end
