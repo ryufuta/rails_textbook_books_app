0b3dbac92ce60b3175d85aa651992ba6fe7a23d0
ryufuta <rfutago@gmail.com>
2024-05-02 16:57:52 +0900

    picture列追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_01_130445) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_02_075211) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
